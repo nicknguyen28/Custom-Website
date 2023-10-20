d5daf0411461be541ea6d95c25181b7aa243bf77
Nicholas Nguyen <nicknguyen3@yahoo.com>
2023-10-20 17:40:00 -0500

    Update nav menu to highlight sections on scroll

---
+++ functions.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const nav_items = document.querySelectorAll('#menu a');
+
+  function highlight_nav_on_scroll() {
+    const scroll_pos = window.scrollY;
+
+    nav_items.forEach(item => {
+      const section_id = item.getAttribute('href').substring(1);
+      const section = document.getElementById(section_id);
+
+      if (section_id === 'home' && scroll_pos < 110) {
+        nav_items.forEach(nav_item => {
+          nav_item.classList.remove('active');
+        });
+
+        item.classList.add('active');
+      }else if (section && section.offsetTop <= scroll_pos + 101 && section.offsetTop + section.offsetHeight > scroll_pos + 101) {
+        nav_items.forEach(nav_item => {
+          nav_item.classList.remove('active');
+        });
+
+        item.classList.add('active');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', highlight_nav_on_scroll);
+  highlight_nav_on_scroll();
+});
+  
